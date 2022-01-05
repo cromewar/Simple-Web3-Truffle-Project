dfbe8e40f148f3b9afbfb6acc1b7cc19ecb98e9d
cromewar <vasiliy@cromewar.com>
2022-01-05 17:57:48 -0500

    testing and readme

--- test/test.js
+++ test/test.js
@@ -1,5 +1,18 @@
 const Hello = artifacts.require("Hello");
 
-module.exports = function (deployer) {
-  deployer.deploy(Hello);
-};
+contract("Hello", (accounts) => {
+  it("Get message", async () => {
+    let instance = await Hello.deployed();
+    const message = await instance.getMessage.call({ from: accounts[0] });
+    assert.equal(message, "Hello world");
+  });
+
+  it("Change message", async () => {
+    let instance = await Hello.deployed();
+    const tx = await instance.setMessage("Cromewar", { from: accounts[1] });
+    console.log(accounts[1]);
+    console.log(tx);
+    const msg = await instance.getMessage.call();
+    assert.equal(msg, "Cromewar");
+  });
+});
